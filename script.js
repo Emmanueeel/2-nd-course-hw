c29e4f1227cfecd2f5d6f2b41bfc111b9df27da3
Emmanueeel <imaanii@mail.ru>
2026-01-06 14:46:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Задание 1
+let password = 'пароль';
+
+
+let userInput = prompt('Введите пароль');
+
+
+if (userInput === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
